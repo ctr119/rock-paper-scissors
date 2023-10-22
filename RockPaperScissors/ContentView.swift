31626cf18646419d98e7b5ae15db498cc174e641
Victor Barrios <victor.barrios@bestsecret.com>
2023-10-23 00:16:27 +0200

    Animate the background

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -12,6 +12,11 @@ struct ContentView: View {
     
     @State private var isGameOver = false
     
+    // Background animation
+    @State var start = UnitPoint(x: 0, y: -2)
+    @State var end = UnitPoint(x: 4, y: 0)
+    private let timer = Timer.publish(every: 1, on: .main, in: .default).autoconnect()
+    
     private let colors = [Color(#colorLiteral(red: 0.9843137255, green: 0.9176470588, blue: 0.6470588235, alpha: 1)), Color(#colorLiteral(red: 1, green: 0.3333333333, blue: 0.6117647059, alpha: 1)), Color(#colorLiteral(red: 0.4156862745, green: 0.7098039216, blue: 0.9294117647, alpha: 1)), Color(#colorLiteral(red: 0.337254902, green: 0.1137254902, blue: 0.7490196078, alpha: 1)), Color(#colorLiteral(red: 0.337254902, green: 0.9215686275, blue: 0.8509803922, alpha: 1))]
     private let playerMoves: [GameMove] = [
         .rock, .paper, .scissors
@@ -20,9 +25,21 @@ struct ContentView: View {
     var body: some View {
         ZStack {
             LinearGradient(gradient: Gradient(colors: colors),
-                           startPoint: .topLeading,
-                           endPoint: .bottomTrailing)
+                           startPoint: start,
+                           endPoint: end)
             .edgesIgnoringSafeArea(.all)
+            .animation(
+                .easeInOut(duration: 10)
+                .repeatForever(autoreverses: true)
+                .speed(0.5),
+                value: start
+            )
+            .onReceive(timer) { _ in
+                self.start = UnitPoint(x: 4, y: 0)
+                self.end = UnitPoint(x: 0, y: 2)
+                self.start = UnitPoint(x: -4, y: 20)
+                self.start = UnitPoint(x: 4, y: 0)
+            }
             
             VStack {
                 Spacer()
