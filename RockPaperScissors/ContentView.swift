77341adc1322a6fc484c2d12f58bd5db1012a916
Victor Barrios <victor.barrios@bestsecret.com>
2023-10-22 12:51:17 +0200

    Add logic to count the rounds and sum points

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -1,9 +1,15 @@
 import SwiftUI
 
 struct ContentView: View {
+    private static let maxRounds = 4
+    
     @State private var opponentMoveIndex = Int.random(in: 0...2)
     @State private var opponentScore = 0
     @State private var playerScore = 0
+    @State private var roundsLeft = Self.maxRounds
+    @State private var roundsIndex = 1
+    
+    @State private var isGameOver = false
     
     private let playerButtonsSpacing: CGFloat = 20
     private let playerMoves: [GameMove] = [
@@ -26,12 +32,37 @@ struct ContentView: View {
                     
                     Spacer()
                     
-                    Text("Move: 1 / 10")
+                    Text("Round: \(roundsIndex) of \(Self.maxRounds)")
                 }
                 .frame(maxWidth: .infinity, maxHeight: .infinity)
             }
         }
         .padding()
+        .alert("Finish!", isPresented: $isGameOver) {
+            Button("Start again", action: restart)
+        } message: {
+            Text(gameOverMessage)
+        }
+    }
+    
+    private func restart() {
+        opponentMoveIndex = Int.random(in: 0...2)
+        
+        playerScore = 0
+        opponentScore = 0
+        
+        roundsLeft = Self.maxRounds
+        roundsIndex = 1
+    }
+    
+    private var gameOverMessage: String {
+        if playerScore == opponentScore {
+            return "Draw! Both got \(playerScore) points"
+        } else if playerScore > opponentScore {
+            return "You win!\nPlayer: \(playerScore)\nOpponent: \(opponentScore)"
+        } else {
+            return "You lose!\nPlayer: \(playerScore)\nOpponent: \(opponentScore)"
+        }
     }
     
     @ViewBuilder
@@ -52,6 +83,17 @@ struct ContentView: View {
     }
     
     private func didTapOnMoveButton(_ playerMove: GameMove) {
+        defer {
+            roundsLeft -= 1
+            
+            if roundsLeft > 0 {
+                opponentMoveIndex = Int.random(in: 0...2)
+                roundsIndex += 1
+            } else {
+                isGameOver = true
+            }
+        }
+        
         guard let opponentMove = GameMove(rawValue: opponentMoveIndex),
               let playerWins = playerMove.wins(opponentMove) else {
             return
