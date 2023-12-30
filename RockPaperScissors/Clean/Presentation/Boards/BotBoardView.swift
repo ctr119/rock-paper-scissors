9c8587eae74c73690333619699301b7eb09b9ddd
Víctor Barrios <sygmaz19@gmail.com>
2023-12-31 00:21:19 +0100

    Polish Bot board

--- RockPaperScissors/Clean/Presentation/Boards/BotBoardView.swift
+++ RockPaperScissors/Clean/Presentation/Boards/BotBoardView.swift
@@ -11,18 +11,24 @@ struct BotBoardView: View {
     @ObservedObject var viewModel: GameViewModel
     
     var body: some View {
-        VStack {
-            Text("- Opponent's points -")
-            Text("\(viewModel.botScore)")
-                .fontWeight(.bold)
-        }
-        .customHeadline()
-        
-        VStack(spacing: 10) {
-            Text("Chosen move")
-                .font(.footnote.monospaced())
-            Text(chosenMove)
-                .font(.largeTitle)
+        VStack(spacing: 25) {
+            VStack {
+                Text("- Opponent's points -")
+                Text("\(viewModel.botScore)")
+                    .fontWeight(.bold)
+            }
+            .customHeadline()
+            
+            VStack(spacing: 10) {
+                Text("Chosen move")
+                    .font(.footnote.monospaced())
+                Text(chosenMove)
+                    .font(.largeTitle)
+                    .padding()
+                    .frame(minWidth: 70)
+                    .background(.gray.opacity(0.8))
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+            }
         }
     }
     
